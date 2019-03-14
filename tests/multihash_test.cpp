757d12351e6bb3990d0f10be034929382dec9af6
Samuel El-Borai <samuel.elborai@gmail.com>
2019-03-14 12:28:28 +0100

    Setup unit tests using Google Test

---
+++ tests/multihash_test.cpp
@@ -0,0 +1,10 @@
+#include "multihash/multihash.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(TestMultihash, NotImplementedYet) {
+	EXPECT_EQ(0, 1) << "Not implemented yet";
+}
+
+} // namespace
